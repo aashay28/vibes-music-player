676d3769eee5143313c719ee8145875f97732a01
Aashay shah <ahs@narola.email>
2022-07-12 17:17:00 +0530

    first commit of music-player

---
+++ src/data.js
@@ -0,0 +1,28 @@
+function chillHop() {
+  return [
+    {
+      name: "Pasoori",
+      cover:
+        "https://res.cloudinary.com/nt-tech/image/upload/v1657624896/musicPlayer/Pasoori-Punjabi-2022-20220203181058-500x500_mwcnyz.jpg",
+      artist: "Ali_Sethi_x_Shae_Gill",
+      audio:
+        "https://res.cloudinary.com/nt-tech/video/upload/v1657624682/musicPlayer/Coke_Studio_Season_14_Pasoori_Ali_Sethi_x_Shae_Gill_s1rywo.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+
+      active: true,
+    },
+    {
+      name: "Despacito",
+      cover:
+        "http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg",
+      artist: "Luis Fonsi",
+      audio:
+        "http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3",
+      color: ["#205950", "#2ab3bf"],
+
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
