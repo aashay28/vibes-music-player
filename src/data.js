814f19b823f35b814b466b5be6ca1b5edc9acfc8
aashay shah <aashayshah28@gmail.com>
2022-07-14 15:29:15 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+function chillHop() {
+  return [
+    {
+      name: "Pasoori",
+      cover:
+        "https://res.cloudinary.com/nt-tech/image/upload/v1657624896/musicPlayer/banner/Pasoori-Punjabi-2022-20220203181058-500x500_mwcnyz.jpg",
+      artist: "Ali_Sethi_x_Shae_Gill",
+      audio:
+        "https://res.cloudinary.com/nt-tech/video/upload/v1657624682/musicPlayer/songs/Coke_Studio_Season_14_Pasoori_Ali_Sethi_x_Shae_Gill_s1rywo.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+
+      active: true,
+    },
+    {
+      name: "40 Minutes Of Hindi Lofi Songs",
+      cover:
+        "https://res.cloudinary.com/nt-tech/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1657686525/musicPlayer/banner/lofi.jpg",
+      artist: "Lofi Girl",
+      audio:
+        "https://res.cloudinary.com/nt-tech/video/upload/v1657646391/musicPlayer/songs/40_Minutes_Of_Hindi_Lofi_Songs_Lof__getmp3.pro_penrzs.mp3",
+      color: ["#205950", "#2ab3bf"],
+
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
